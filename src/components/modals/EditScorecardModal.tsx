979343753e4744a459e5269ac5ad73bec73ebd01
Nathan Betts <nathanbetts@hotmail.co.uk>
2023-01-21 01:51:51 +0000

    improve scorecard modal info

--- src/components/modals/EditScorecardModal.tsx
+++ src/components/modals/EditScorecardModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Checkbox, Flex, NumberInput, Select } from '@mantine/core';
+import { Affix, Badge, Button, Card, Center, Checkbox, Flex, NumberInput, Select, Text, Transition } from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import { closeAllModals } from '@mantine/modals';
@@ -39,18 +39,15 @@ export const EditScorecardModal = ({ scorecard }: EditScorecardModalProps) => {
   }
 
   const courseOptions = courses.data.sort((a, b) => a.name.localeCompare(b.name)).map((course) => ({ value: course.id, label: course.name }));
-  const holeCount = courses.data.find((course) => course.id === form.values.courseId)?.holes.length || 0;
-
-  const holeFields = new Array(holeCount)
-    .fill(0)
-    .map((_item, index) => <NumberInput key={index} min={0} label={`Hole ${index + 1}`} {...form.getInputProps(`scores.${index}`)} />);
+  const course = courses.data.find((course) => course.id === form.values.courseId);
+  const totalScore = form.values.scores.reduce((prev, current) => (typeof prev === 'string' ? parseInt(prev) : prev) + current, 0);
 
   const submitForm = (values: FormInputs) => {
     mutation.mutate(
       {
         timestamp: Timestamp.fromDate(values.date),
         courseId: values.courseId,
-        scores: new Array(holeCount).fill(0).map((_, index) => values.scores[index] || 0),
+        scores: new Array(course?.holes.length || 18).fill(0).map((_, index) => values.scores[index] || 0),
         hidden: values.hidden,
       },
       {
@@ -64,11 +61,30 @@ export const EditScorecardModal = ({ scorecard }: EditScorecardModalProps) => {
       <Flex direction="column">
         <DatePicker label="Date" placeholder="Choose date" {...form.getInputProps('date')} maxDate={new Date()} data-autofocus />
         {courseOptions && <Select label="Course" placeholder="Choose course" data={courseOptions} {...form.getInputProps('courseId')} />}
-        <Card shadow="sm" radius="md" withBorder p="sm">
-          <Flex direction="column" gap="xs">
-            {holeFields}
-          </Flex>
-        </Card>
+        {course && (
+          <Card shadow="sm" radius="md" withBorder p="sm">
+            <Flex direction="column" gap="sm">
+              {course.holes.map((hole, index) => (
+                <NumberInput
+                  key={index}
+                  min={0}
+                  label={
+                    <Flex align="center">
+                      {`Hole ${index + 1}`}
+                      <Badge color="green" variant="light">
+                        Par {hole.par}
+                      </Badge>
+                      <Badge color="pink" variant="light">
+                        {hole.yards} yards
+                      </Badge>
+                    </Flex>
+                  }
+                  {...form.getInputProps(`scores.${index}`)}
+                />
+              ))}
+            </Flex>
+          </Card>
+        )}
         <Checkbox mt="sm" label="Hide this scorecard from other players" {...form.getInputProps('hidden', { type: 'checkbox' })} />
         <Flex justify="center">
           <Button type="submit" mt="md" disabled={mutation.isLoading}>
@@ -76,6 +92,22 @@ export const EditScorecardModal = ({ scorecard }: EditScorecardModalProps) => {
           </Button>
         </Flex>
       </Flex>
+      <Affix position={{ top: 20, left: 0 }} zIndex={202} style={{ width: '100%' }}>
+        <Transition transition="slide-up" mounted={totalScore > 0}>
+          {(transitionStyles) => (
+            <Center>
+              <Card style={transitionStyles} bg="blue.6" p="sm" px="md" shadow="sm">
+                <Flex gap="xs">
+                  <Text color="white">Total score:</Text>
+                  <Text color="white" weight="bold">
+                    {totalScore}
+                  </Text>
+                </Flex>
+              </Card>
+            </Center>
+          )}
+        </Transition>
+      </Affix>
     </form>
   );
 };
