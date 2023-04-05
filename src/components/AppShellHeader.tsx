2f691f5539838902e819e26459e3eede676ee6ab
Nathan Betts <nathanbetts@hotmail.co.uk>
2023-04-05 22:21:38 +0100

    remove github link

--- src/components/AppShellHeader.tsx
+++ src/components/AppShellHeader.tsx
@@ -1,5 +1,5 @@
 import { Header, MediaQuery, Burger, Tooltip, ActionIcon, Text, useMantineColorScheme, Flex } from '@mantine/core';
-import { IconSun, IconMoon, IconBrandGithub, IconExternalLink } from '@tabler/icons';
+import { IconSun, IconMoon } from '@tabler/icons';
 
 type AppShellHeaderProps = {
   navMenuOpened: boolean;
@@ -27,21 +27,6 @@ export const AppShellHeader = ({ navMenuOpened, onNavMenuToggle }: AppShellHeade
               {colorScheme === 'dark' ? <IconSun size={20} /> : <IconMoon size={20} />}
             </ActionIcon>
           </Tooltip>
-          <MediaQuery smallerThan="xs" styles={{ display: 'none' }}>
-            <Tooltip
-              withArrow
-              label={
-                <Flex align="center" gap={5}>
-                  <Text>GitHub</Text>
-                  <IconExternalLink size={12} />
-                </Flex>
-              }
-            >
-              <ActionIcon variant="default" size={32} radius="md" component="a" href="https://github.com/nbetts/golf-tracker" target="_blank">
-                <IconBrandGithub size={20} />
-              </ActionIcon>
-            </Tooltip>
-          </MediaQuery>
         </Flex>
       </Flex>
     </Header>
